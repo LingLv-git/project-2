061c95ff58f25f3bf48edf176ed6278adc9c9551
LingLv-git <55591800+LingLv-git@users.noreply.github.com>
2021-09-16 10:38:03 -0400

    updated query.sql to add column alias and result.png

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
-select aa.name, TRUNC(aa.average_rating::numeric,2), aa. total_reviews from (
+select aa.name, TRUNC(aa.average_rating::numeric,2) average_rating, aa. total_reviews from (
 select r.name, (a.reviews * a.rating + b.reviews * b.rating)/(a.reviews + b.reviews) average_rating, a.reviews + b.reviews as total_reviews from restaurants r
 join restaurant_info a
 on a.restaurant_id = r.id
